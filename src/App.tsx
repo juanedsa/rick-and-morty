08857f66145a7bec518c1e6e831ce20dc835f3cf
Juan Salazar <juanedsa@gmail.com>
2023-05-05 10:15:22 -0500

    :art: Add Container CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
 
+import { HomePage } from './pages/HomePage'
+import { DetailPage } from './pages/DetailPage'
+
 function App() {
-  return <>Hello Rick and Morty</>
+  return (
+    <main className='app-container'>
+      <section></section>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+        </Routes>
+      </HashRouter>
+      <section></section>
+    </main>
+  )
 }
 
 export default App
